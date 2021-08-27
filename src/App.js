292bbcc7cf839e86391eb6b32582414d6096effc
Melchor Dominguez <Mdmngz411@gmail.com>
2021-08-27 03:45:24 -0400

    - #1: add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.scss';
 
 import { Home } from './pages';
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
