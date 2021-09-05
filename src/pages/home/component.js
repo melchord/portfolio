91d05275ed969db80b1056482475879f589f15d3
Melchor Dominguez <Mdmngz411@gmail.com>
2021-09-05 02:00:47 -0400

    - #1: add database component

--- src/pages/home/component.js
+++ src/pages/home/component.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Layout, Navbar } from '../../components';
+import { Firebase, Layout, Navbar } from '../../components';
 
 const Home = () => {
+  const db = Firebase.firestore();
+  const header = db.collection('Pages').doc('Home').get()
+		.then(snapshot => {
+			snapshot
+			.data
+			.toString
+		}
+	);
+  console.log(header);
   return (
     <Layout>
       <div className='home'>
         <Navbar />
-        <h1>I am Melchor Dominguez</h1>
-				<h2></h2>
+        <h1>{header}</h1>
+        <h2>Software Engineer locaed in Charlotte.</h2>
+        <h2>
+          Experienced in <span>Javascript</span>
+        </h2>
       </div>
     </Layout>
   );
